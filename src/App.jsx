a3b1168018b0eee51a90f2df3d9fc539caaa0cbc
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-05-06 11:57:10 -0300

    refactor: organizando estrutura

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
+import { Button } from "@mui/material";
+
 export const App = () => {
   return (
-    <main>
-      <form>
-        <header>
-          <div className="logo">Logo da Player</div>
-          <h1>Calculando Preço - Lucro Real</h1>
-        </header>
-        <div>
-          <p><strong>Aviso:</strong> Essa calculadora pode apresentar pequenas divergências nos resultados. para maior precisão, <a href="#">consulte um dos nossos profissionais</a></p>
-        </div>
-        <div>
-          <h2>Produto</h2>
+    <>
+      <header>
+        <div className="logo">Logo da Player</div>
+        <h1>Calculando Preço - Lucro Real</h1>
+      </header>
+      <main>
+        <form>
+          <div>
+            <p>
+              <strong>Aviso:</strong> Essa calculadora pode apresentar pequenas
+              divergências nos resultados. para maior precisão,{" "}
+              <a href="#">consulte um dos nossos profissionais</a>
+            </p>
+          </div>
+          <div>
+            <h2>Produto</h2>
             <label>Preço de Compra</label>
             <input type="number" />
-        </div>
-      </form>
-    </main>
+          </div>
+          <Button variant="contained">Contained</Button>
+        </form>
+      </main>
+      <footer>
+        <p>Player Contabilidade © 2024 - Todos direitos reservados</p>
+      </footer>
+    </>
   );
 };
-
-
