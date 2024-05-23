7ab9979be0d3f5f2229940026b8fd983d00c58ec
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-05-23 16:57:41 -0300

    chore: criando componente TextGenerator

---
+++ src/components/TextGenerator/index.jsx
@@ -0,0 +1,16 @@
+export const TextGenerator = () => {
+  return (
+    <p
+      style={{
+        fontSize: "1rem",
+        textAlign: "center",
+        color: "#0D253D",
+        fontFamily: "roboto",
+        fontWeight: "bold",
+      }}
+    >
+      Torne mais acessível a análise financeira de sua empresa ao simplificar o
+      cálculo do Lucro Real.
+    </p>
+  );
+};
