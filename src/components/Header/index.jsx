326d8bf225c3614fa93d44ad445949abc5d51351
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-05-23 17:02:48 -0300

    refactore: refatorando estilos

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,22 +3,33 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import logo from "../../../public/img/logo.png";
+import { Container } from "@mui/material";
 
 export const Header = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar style={{ backgroundColor: "#0D253D" }} position="static">
-        <Toolbar>
-          <img
-            src={logo}
-            alt="logo"
-            style={{ width: "130px", marginRight: "20px" }}
-          />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Calculando Preço - Lucro Real
-          </Typography>
-        </Toolbar>
-      </AppBar>
+    <Box sx={{ backgroundColor: "#0D253D", flexGrow: 1 }}>
+      <Container maxWidth="lg">
+        <AppBar
+          style={{ backgroundColor: "#0D253D", textAlign: "center" }}
+          position="static"
+          elevation={0}
+        >
+          <Toolbar>
+            <img
+              src={logo}
+              alt="logo"
+              style={{ width: "130px", marginRight: "20px" }}
+            />
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, fontSize: "25px" }}
+            >
+              Calculando Preço - Lucro Real
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Container>
     </Box>
   );
 };
