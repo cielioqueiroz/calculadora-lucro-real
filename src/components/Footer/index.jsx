a95a890ff4fdb2385e5aa5e1dc785266e8c23322
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-06-03 18:58:23 -0300

    refactoring projets

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,6 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
-
+import { AppBar, Toolbar, Typography, Grid, Box } from "@mui/material";
+import logo from "../../../public/img/logo.png";
 export const Footer = () => {
- 
   function getYear() {
     const date = new Date();
     const year = date.getFullYear();
@@ -11,27 +10,48 @@ export const Footer = () => {
   return (
     <AppBar
       sx={{
-        top: 'auto',
+        top: "auto",
         bottom: 0,
         backgroundColor: "#0D253D",
         textAlign: "center",
+        padding: "4px 0",
       }}
       position="static"
       color="primary"
     >
       <Toolbar>
-        <Typography
-          sx={{
-            flexGrow: 1,
-            fontSize: "18px",
-            color: "#ffffff",
-            textAlign: "center",
-          }}
-          variant="body1"
-          color="inherit"
-        >
-          Player Contabilidade © {getYear()} - Todos os direitos reservados.
-        </Typography>
+        <Grid container sx={{ width: "100%", alignItems: "center" }}>
+          <Grid item xs={5} sx={{ textAlign: "left" }}>
+            <Typography
+              sx={{
+                fontSize: "18px",
+                color: "#ffffff",
+                fontWeight: "normal",
+              }}
+              variant="body1"
+              color="inherit"
+            >
+              © {getYear()} Player Contabilidade. All rights reserved.
+            </Typography>
+          </Grid>
+          <Grid item xs={2} sx={{ textAlign: "center" }}>
+            <Box component="img" src={logo} alt="Logo" sx={{ height: 40 }} />{" "}
+            {/* Ajuste o tamanho conforme necessário */}
+          </Grid>
+          <Grid item xs={5} sx={{ textAlign: "right" }}>
+            <Typography
+              sx={{
+                fontSize: "18px",
+                color: "#ffffff",
+                fontWeight: "normal",
+              }}
+              variant="body1"
+              color="inherit"
+            >
+              CNPJ 33.036.816/0001-24 | CRC -TO 000580/0-8 | CRA-TO 20-03828
+            </Typography>
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
